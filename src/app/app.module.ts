4868547bcc9e31c3aa40ea2d3fa92127a0604287
Vít Ludwig <vit.ludwig1@gmail.com>
2023-07-02 16:39:12 +0200

    onesignal import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,14 +51,14 @@ export class MyMissingTranslationHandler implements MissingTranslationHandler {
 			},
 			missingTranslationHandler: {provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler},
 		}),
-		ServiceWorkerModule.register('ngsw-worker.js', {
-			enabled: true,
-			// Register the ServiceWorker as soon as the application is stable
-			// or after 30 seconds (whichever comes first).
-			registrationStrategy: 'registerWhenStable:30000'
-		}),
-		ServiceWorkerModule.register('notification.worker.js', { enabled: true, registrationStrategy: 'registerWhenStable:30000' }),
-		ServiceWorkerModule.register('OneSignalSDKWorker.js', { enabled: true, registrationStrategy: 'registerWhenStable:30000' }),
+		// ServiceWorkerModule.register('ngsw-worker.js', {
+		// 	enabled: true,
+		// 	// Register the ServiceWorker as soon as the application is stable
+		// 	// or after 30 seconds (whichever comes first).
+		// 	registrationStrategy: 'registerWhenStable:30000'
+		// }),
+		// ServiceWorkerModule.register('notification.worker.js', { enabled: true, registrationStrategy: 'registerWhenStable:30000' }),
+		// ServiceWorkerModule.register('OneSignalSDKWorker.js', { enabled: true, registrationStrategy: 'registerWhenStable:30000' }),
 	],
 	providers: [
 		{provide: APP_INITIALIZER, useFactory: appInitializerFactory, deps: [ProgramService], multi: true},
