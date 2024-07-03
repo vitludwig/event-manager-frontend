4f60e66832ad453590d87d38c259e1d727ec6438
Vít Ludwig <vit.ludwig1@gmail.com>
2024-07-03 11:13:45 +0200

    changed one signal api keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	oneSignalApiKey: "OTc1MTgwMWMtNDY5Yy00ZjM2LWI5YjEtNWYyZGE5ZDA4MzNh",
-	oneSignalAppId: "31a5956f-5e42-4af4-a178-6da520ecac86",
+	oneSignalApiKey: "NDcwZWY5YTUtYWQyMy00NTY1LWIxZDEtNzRjYTAxZDAyYzJh",
+	oneSignalAppId: "14d3b005-8f00-478a-b84e-14807cf26f99",
 };
