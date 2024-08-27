b4c1a6f73f4ff95aaf1951aa6fe05479ea13a4d2
Vít Ludwig <vit.ludwig1@gmail.com>
2024-08-27 20:16:07 +0200

    removed env keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	oneSignalApiKey: "ZjZjMjBkOGYtZDliYi00NGY1LTgxMGItMjk2MzM0MzNkMjg5",
-	oneSignalAppId: "260dd1c8-ce92-4821-a870-1c5f88439bf5",
+	oneSignalApiKey: "",
+	oneSignalAppId: "",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	oneSignalApiKey: "NDcwZWY5YTUtYWQyMy00NTY1LWIxZDEtNzRjYTAxZDAyYzJh",
-	oneSignalAppId: "14d3b005-8f00-478a-b84e-14807cf26f99",
+	oneSignalApiKey: "",
+	oneSignalAppId: "",
 };
