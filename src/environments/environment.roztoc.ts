c448adb109212f172a0dd246edd7c89616fb303f
Vít Ludwig <vit.ludwig1@gmail.com>
2025-08-06 21:38:22 +0200

    fixed api url

--- src/environments/environment.roztoc.ts
+++ src/environments/environment.roztoc.ts
@@ -2,7 +2,7 @@ export const environment = {
 	oneSignalApiKey: "NDcwZWY5YTUtYWQyMy00NTY1LWIxZDEtNzRjYTAxZDAyYzJh",
 	oneSignalAppId: "655aca75-1bc7-4a63-b9d6-0d0127a4b92d",
 	wsUrl: "signalr/events",
-	apiUrl: "",
+	apiUrl: "/api/v1",
 	faqUrl_CS: "https://roztocfest.com/faq/?lang=cs",
 	faqUrl_EN: "https://roztocfest.com/faq/?lang=en",
 	festivalId: 'ROZTOCFEST'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 	oneSignalApiKey: "NDcwZWY5YTUtYWQyMy00NTY1LWIxZDEtNzRjYTAxZDAyYzJh",
 	oneSignalAppId: "14d3b005-8f00-478a-b84e-14807cf26f99",
 	wsUrl: "signalr/events",
-	apiUrl: "",
+	apiUrl: "/api/v1",
 	faqUrl_CS: "https://docs.google.com/document/d/1StVQlwSWorjiQyfoLuckcz8geOw3uurjPaC8ni5jwbQ/edit?usp=sharing",
 	faqUrl_EN: "https://docs.google.com/document/d/1UGDLkfL1gM7RnHSdhyqBucoIrqRw9k5MxgfKodPGEAE/edit?usp=sharing",
 	festivalId: 'RUSTHAVEN'
