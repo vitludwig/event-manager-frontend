a9ba68f83e6bd8c67e8ca24c8592b83796582437
Vít Ludwig <vit.ludwig@ceosdata.com>
2026-02-15 13:37:47 +0100

    load active them version

--- src/app/common/services/theme/theme.service.ts
+++ src/app/common/services/theme/theme.service.ts
@@ -9,14 +9,13 @@ export class ThemeService {
     private document = inject(DOCUMENT);
     private http = inject(HttpClient);
 
-    // Keep track of the current theme link element so we can remove/update it
-    private currentThemeLink: HTMLLinkElement | null = null;
-
     async loadThemeBundle(): Promise<void> {
         const head = this.document.getElementsByTagName('head')[0];
         let activeTheme;
+        let activeThemeVersion;
         try {
             activeTheme = await firstValueFrom(this.http.get(`${environment.signalrUrl}/public/themes/active-theme.txt`, {responseType: 'text'}));
+            activeThemeVersion = await firstValueFrom(this.http.get(`${environment.signalrUrl}/public/themes/active-theme-version.txt`, {responseType: 'text'}));
         } catch(e) {
             activeTheme = 'rzb-theme';
         }
@@ -24,21 +23,16 @@ export class ThemeService {
         if(!activeTheme) {
             activeTheme = 'rzb-theme';
         }
-
-        if (this.currentThemeLink) {
-            this.currentThemeLink.href = `${environment.signalrUrl}/public/themes/${activeTheme}.css`;
-        } else {
-            const style = this.document.createElement('link');
-            style.id = 'client-theme';
-            style.rel = 'stylesheet';
-            style.href = `${environment.signalrUrl}/public/themes/${activeTheme}.css`;
-            style.onerror = () => {
-                style.onerror = null;
-                style.href = '/assets/themes/rzb-theme.css';
-            }
-
-            head.appendChild(style);
-            this.currentThemeLink = style;
+        const style = this.document.createElement('link');
+        style.id = 'client-theme';
+        style.rel = 'stylesheet';
+        style.href = `${environment.signalrUrl}/public/themes/${activeTheme}.css?v=${activeThemeVersion}`;
+        style.onerror = () => {
+            style.onerror = null;
+            style.href = '/assets/themes/rzb-theme.css';
         }
+
+        head.appendChild(style);
+
     }
 }
