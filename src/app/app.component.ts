d8b215596eb5e6c911ecaefdeab984e8bc50c612
Vít Ludwig <vit.ludwig@ceosdata.com>
2026-03-04 20:09:46 +0100

    fix back button to go back in app instead closing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, NgZone, OnInit} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {TranslateService} from '@ngx-translate/core';
 import {NotificationService} from './modules/notifications/services/notification/notification.service';
@@ -6,16 +6,19 @@ import {ProgramService} from './modules/program/services/program/program.service
 import dayjs from 'dayjs';
 import {filter} from 'rxjs';
 import {NavigationEnd, Router} from '@angular/router';
+import {Location} from '@angular/common';
 import {ERoute} from './common/types/ERoute';
 import {ELocalNotificationAction} from "./modules/notifications/types/ILocalNotificationPayload";
 import {SettingsService} from "./common/services/settings/settings.service";
 import {EDisplayDevice} from "./common/types/EDisplayDevice";
+import {App} from '@capacitor/app';
+import {Capacitor} from '@capacitor/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent implements OnInit {
 
@@ -23,7 +26,10 @@ export class AppComponent implements OnInit {
 	private readonly notificationService: NotificationService = inject(NotificationService);
 	private readonly programService: ProgramService = inject(ProgramService);
 	private readonly router: Router = inject(Router);
+	private readonly location: Location = inject(Location);
 	private readonly destroyRef: DestroyRef = inject(DestroyRef);
+	private readonly ngZone: NgZone = inject(NgZone);
+	private readonly dialog: MatDialog = inject(MatDialog);
 	protected readonly settingsService: SettingsService = inject(SettingsService);
 
 	protected EDisplayDevice = EDisplayDevice;
@@ -33,6 +39,7 @@ export class AppComponent implements OnInit {
 	public async ngOnInit(): Promise<void> {
 		this.handleLanguage();
 		this.initLocalNotifications();
+		this.initHardwareBackButton();
 
 		this.handleSubscriptionBtn();
 
@@ -76,6 +83,28 @@ export class AppComponent implements OnInit {
 
 		this.translate.setDefaultLang(language);
 		this.translate.use(language ?? this.translate.defaultLang);
+	}
+
+	private initHardwareBackButton(): void {
+		if(!Capacitor.isNativePlatform()) {
+			return;
+		}
+
+		App.addListener('backButton', () => {
+			this.ngZone.run(() => {
+				if(this.dialog.openDialogs.length > 0) {
+					this.dialog.openDialogs[this.dialog.openDialogs.length - 1].close();
+					return;
+				}
+
+				const url = this.router.url;
+				if(url === '/' || url === `/${ERoute.PROGRAM}`) {
+					App.exitApp();
+				} else {
+					this.location.back();
+				}
+			});
+		});
 	}
 
 	private initLocalNotifications(): void {
